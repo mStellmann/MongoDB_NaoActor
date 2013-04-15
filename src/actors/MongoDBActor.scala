374c18375a3028c6902c537e9f31b2f0a953815e
mStellmann <matthias.stellmann@haw-hamburg.de>
2013-04-15 13:06:49 +0200

    First Commit - Let's do this!

---
+++ src/actors/MongoDBActor.scala
@@ -0,0 +1,12 @@
+package actors
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nao
+ * Date: 15.04.13
+ * Time: 12:59
+ * To change this template use File | Settings | File Templates.
+ */
+class MongoDBActor {
+
+}
