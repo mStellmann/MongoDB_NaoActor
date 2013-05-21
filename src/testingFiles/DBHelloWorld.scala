da6ca835114614b2000ccee2c97122800fda2156
Gregsta <g.markiewicz@hotmail.de>
2013-05-21 11:33:59 +0200

    msg an nao actor

--- src/testingFiles/DBHelloWorld.scala
+++ src/testingFiles/DBHelloWorld.scala
@@ -8,10 +8,26 @@ import naogateway.value.NaoMessages._
 import naogateway.value.NaoMessages.Conversions._
 import naogateway.value.NaoVisionMessages._
 
+import akka.actor.ActorSystem
+import akka.actor.Actor
+import akka.event.Logging
+import com.typesafe.config.ConfigFactory
+import akka.actor.ActorRef
+import akka.actor.Props
+import naogateway.value.NaoMessages._
+import naogateway.value.NaoMessages.Conversions._
+import naogateway.value.NaoVisionMessages._
+
 object DBHelloWorld extends App {
 
+
+  val config = ConfigFactory.load()
+  val system = ActorSystem("remoting", config.getConfig("remoting").withFallback(config))
+
+  val naoActor = system.actorFor("akka://naogateway@192.168.1.100:2550/user/hanna")
+
   // Create the Akka system
-  val system = ActorSystem("DBSystem")
+ // val system = ActorSystem("DBSystem")
   system.actorOf(Props[DBConfigurator], name = "DBConfigurator")
 
   val agent = system.actorSelection("/user/DBConfigurator/DBAgent")
@@ -28,12 +44,14 @@ object DBHelloWorld extends App {
     var fileActor: ActorRef = null
 
     // Getting the Database Actors
-    override def preStart = agent ! DatabaseActors
+    override def preStart = agent ! DatabaseActors; naoActor ! Connect
+    Thread.sleep(2000)
+    var noresponseA: ActorRef = self
 
     def receive = {
       // receiving the SerialNumbers (names) for each robot
       case ReceivedDatabaseActors(cActor, fActor) => {
-        println("received Actors ")
+
         commandActor = cActor
         fileActor = fActor
         sender ! RobotSerialNumbers
@@ -41,17 +59,22 @@ object DBHelloWorld extends App {
 
       // receiving the SerialNumbers and starting the Test
       case ReceivedRobotSerialNumbers(rsnAry) => {
-        println("Received serials ")
+
         commandActor ! SaveCommand(rsnAry(1), System.currentTimeMillis(), Call('ALTextToSpeech, 'say, List("Stehen bleiben!")), List("Gespraech", "Uni", "Datenbank", "Test"))
 
         commandActor ! SearchCommand(rsnAry(1), commandList = Option(List("TextToSpeech")))
         commandActor ! SearchCommand(rsnAry(1), tagList = Option(List("Gespraech", "Uni", "Datenbank", "Test")))
       }
 
+      case (response: ActorRef, noResponse: ActorRef, vision: ActorRef) => {
+        noresponseA = noResponse
+      }
+
+
       case ReceivedCommand(commandList) =>
-        println("Receive : " + commandList)
+
         commandList match {
-          case Left(callList) => for (elem <- callList) println(elem)
+          case Left(callList) => for (elem <- callList) noresponseA ! elem
           case Right(errMsg) => println(errMsg)
         }
     }
