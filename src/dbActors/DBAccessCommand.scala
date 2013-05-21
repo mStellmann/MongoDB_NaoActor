9a322fdc9723f72417ac975e00955b2dad7bf88a
Gregsta <g.markiewicz@hotmail.de>
2013-05-21 09:23:04 +0200

    receivedData

--- src/dbActors/DBAccessCommand.scala
+++ src/dbActors/DBAccessCommand.scala
@@ -2,8 +2,21 @@ package dbActors
 
 import akka.actor.{ActorRef, Actor}
 import messages.userMessages._
-import messages.internalMessages.{Save, SearchData, ReceivedData}
+import messages.internalMessages.{SaveCommand, Save, SearchData, ReceivedData}
 import scala.util.{Try, Success, Failure}
+import messages.userMessages.SaveCommand
+import naogateway.value.Hawactormsg.MixedValue
+import com.mongodb.casbah.Imports._
+import scala.util.Failure
+import messages.userMessages.SearchCommand
+import messages.internalMessages.SearchData
+import messages.internalMessages.Save
+import scala.util.Success
+import messages.internalMessages.ReceivedData
+import messages.userMessages.SaveCommand
+import messages.internalMessages.SaveCommand
+import naogateway.value.NaoMessages.Call
+
 /**
  * This Actor provides the functionality to logging and reading commands from a Nao-Robot.
  */
@@ -18,8 +31,16 @@ class DBAccessCommand extends Actor {
 
   def receive = {
     // TODO - ScalaDoc
-    case SaveCommand(robotSerialNumber, timestamp, call, tagList) => mongoDBActor ! Save(call.module.name, robotSerialNumber, timestamp, tagList)
+    //case SaveCommand(robotSerialNumber, timestamp, call, tagList) => mongoDBActor ! Save(call.module.name, robotSerialNumber, timestamp, tagList)
+    case SaveCommand(robotSerialNumber, timestamp, call, tagList) => {
 
+      val mongoDBDoc = Map(
+        "callModule" -> List(call.module.name.toString),
+        "callMethod" -> List(call.method.name.toString),
+        "callArgs" -> unpackMixedVals(call.parameters))
+
+      mongoDBActor ! Save(call.module.name, robotSerialNumber, timestamp, mongoDBDoc ++ content)
+    }
 
 
     // TODO - ScalaDoc
@@ -33,15 +54,63 @@ class DBAccessCommand extends Actor {
 
     // TODO - ScalaDoc
     case ReceivedData(dataList, origin) => dataList match{
-      case Success(list) => {
-        for(doc <- list) doc.keys.foreach(|elem|
-          origin ! doc.get(elem))
-      }
-      case Failure(list) => {
 
+
+      case Success(list) => {
+        val commands = for (entry <- list) yield {
+          if (entry.contains("callModule")) {
+            val callModule: Symbol = Symbol.apply(entry("callModule")(0).asInstanceOf[String])
+            val callMethod: Symbol = Symbol.apply(entry("callMethod")(0).asInstanceOf[String])
+            val callArgs: List[MixedValue] = dbTypesToMixedVals(entry("callArgs"))
+            Call(callModule, callMethod, callArgs)
+
+          }
+        }
+        origin ! ReceivedCommand(Either(Left(commands)))
+      }
+
+      case Failure(list) => {
+                              origin ! ReceivedCommand(Either(Right("Error")))
       }
     } // TODO
 
 
+  }
+
+
+  def unpackMixedVals(list: List[MixedValue]): List[Any] = {
+    for (arg <- list) yield {
+      if (arg.hasInt) arg.getInt
+      else if (arg.hasFloat()) arg.getFloat
+      else if (arg.hasBool()) arg.getBool
+      else if (arg.hasString()) arg.getString
+      else if (arg.hasBinary()) arg.getBinary
+      else if (arg.getArrayCount() > 0) {
+        import scala.collection.JavaConversions._
+        unpackMixedVals(arg.getArrayList().toList)
+      }
+    }
+  }
+
+  def dbTypesToMixedVals(list: List[Any]): List[MixedValue] = {
+    import naogateway.value.NaoMessages.Conversions._
+    for (arg <- list) yield {
+      arg match {
+        case x: Int => int2Mixed(x)
+        case x: Double => float2Mixed(x.toFloat)
+        case x: Boolean => bool2Mixed(x)
+        case x: String => string2Mixed(x)
+        //        case x: Byte => x
+        case x: BasicDBList => {
+          val floatList = for (i <- 0 until x.size()) yield {
+            x.toList(i) match {
+              case d: Double => d.toFloat
+              case x => x
+            }
+          }
+          anyToMixedVal(floatList)
+        }
+      }
+    }
   }
 }
